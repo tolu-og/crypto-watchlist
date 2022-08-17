2efb4989d277875a1b6dbae8cd2ba3e97c521ddb
tolzydrill <37539097+tolzydrill@users.noreply.github.com>
2022-08-17 15:39:29 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import { ThemeProvider } from './context/ThemeContext';
-import Home from './routes/Home';
-import Signin from './routes/Signin';
-import Signup from './routes/Signup';
-import Account from './routes/Account';
-import axios from 'axios';
-import CoinPage from './routes/CoinPage';
-import Footer from './components/Footer';
-import { AuthContextProvider } from './context/AuthContext';
+import React, { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { ThemeProvider } from "./context/ThemeContext";
+import Home from "./routes/Home";
+import Signin from "./routes/Signin";
+import Signup from "./routes/Signup";
+import Account from "./routes/Account";
+import axios from "axios";
+import CoinPage from "./routes/CoinPage";
+import Footer from "./components/Footer";
+import { AuthContextProvider } from "./context/AuthContext";
+import Helmet from "react-helmet";
 
 function App() {
   const [coins, setCoins] = useState([]);
 
   const url =
-    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true';
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true";
 
   useEffect(() => {
     axios.get(url).then((response) => {
@@ -27,14 +28,18 @@ function App() {
   return (
     <ThemeProvider>
       <AuthContextProvider>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Crypto Price Tracker</title>
+        </Helmet>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home coins={coins} />} />
-          <Route path='/signin' element={<Signin />} />
-          <Route path='/signup' element={<Signup />} />
-          <Route path='/account' element={<Account />} />
-          <Route path='/coin/:coinId' element={<CoinPage />}>
-            <Route path=':coinId' />
+          <Route path="/" element={<Home coins={coins} />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/coin/:coinId" element={<CoinPage />}>
+            <Route path=":coinId" />
           </Route>
         </Routes>
         <Footer />
